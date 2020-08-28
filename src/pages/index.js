dfef94863d161c7a315ca09191223ed4b703f4dc
John Desrosiers <john.desro@gmail.com>
2020-08-28 14:27:45 -0700

    Initial gatsby setup

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <blockquote
+      className="blockquote"
+      cite="https://youtu.be/bDf4O0-XV7I?t=1049"
+    >
+      <p className="blockquote__body">
+        If you only practice what people teach you, then you'll only know what
+        they know.
+      </p>
+      <footer className="blockquote__byline">&ndash; Leo Pelligrino</footer>
+    </blockquote>
+  );
+}
