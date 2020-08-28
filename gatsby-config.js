d52ba775d0c64c0c3daa55c66feff25de73cd20f
John Desrosiers <john.desro@gmail.com>
2020-08-28 15:50:45 -0700

    Configure manifest and sw for pwa functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
   pathPrefix: "/quote",
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Quote",
+        short_name: "Quote",
+        start_url: "/quote",
+        theme_color: "#86232d",
+        display: "standalone",
+        icon: "src/icon.png",
+      },
+    },
+    "gatsby-plugin-offline",
+  ],
 };
