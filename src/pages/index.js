43ba41e388105c1e81fef2536c140ea09caf9b84
John Desrosiers <john.desro@gmail.com>
2020-08-29 05:37:04 -0700

    Create page component for quote layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Blockquote from "../components/Blockquote";
+import Page from "../components/Page";
 import quotes from "../quotes";
 
 export default function Home() {
-  return <Blockquote {...quotes[1]} />;
+  return (
+    <Page>
+      <Blockquote {...quotes[1]} />
+    </Page>
+  );
 }
